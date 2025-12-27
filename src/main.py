9719e09a3b238219d133d501151c4f58b9d778cd
Nathan Chambers <chambersnathanm@gmail.com>
2025-12-27 14:51:43 +0800

    prep for GitHub hosting

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
-def main(): 
-    
-    test_node = TextNode("Hello there!", TextType.TEXT, "http:localhost:8888")
-    
-    print(repr(test_node))
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
+
+def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+
 
 main()
